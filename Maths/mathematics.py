fd9b8f1ac8024f3fbc8fd3d33b71a3ba54a38e22
gen-06 <genalpha089@gmail.com>
2025-10-21 08:59:01 -0400

    math_added

---
+++ Maths/mathematics.py
@@ -0,0 +1,19 @@
+#sum or addition function
+def summation(a, b):
+    result = a + b
+    return result
+
+
+
+#subtraction function
+def subtraction(a, b):
+    result = a - b
+    return result
+
+
+
+
+#multiplication function
+def multiplication(a, b):
+    result = a * b
+    return result
